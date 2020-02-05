8dff469aa66c2ed1321c2289c6508ab210a1ebfa
Mikołaj <mikolaj.gronek@gmail.com>
2020-02-05 18:24:09 +0100

    first push

--- client/src/app/model/expense.ts
+++
@@ -1,5 +0,0 @@
-export class Expense {
-    id:string;
-    amount:string;
-    category:string;
-}
--- restexpenses/src/main/java/com/restexpenses/restexpenses/controllers/ExpenseController.java
+++ restexpenses/src/main/java/com/restexpenses/restexpenses/controllers/ExpenseController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+//@CrossOrigin(origins = "https://restexpenses.herokuapp.com")
 @CrossOrigin(origins = "http://localhost:4200")
 public class ExpenseController {
 
